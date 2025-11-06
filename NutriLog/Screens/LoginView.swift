636640f654c1fa39c99cb9519644bb022f4f183c
Extrajulien <julien.lamothe01@gmail.com>
2025-11-06 09:47:20 -0500

    feat: Implement login view

--- NutriLog/Screens/LoginView.swift
+++ NutriLog/Screens/LoginView.swift
@@ -1,11 +1,54 @@
 import SwiftUI
+import LocalAuthentication
+import Foundation
 
 struct LoginView: View {
+    
+    @Binding var isAuthentificated: Bool
+    
     var body: some View {
-        Text("Vue Connexion")
+        if (isAuthentificated) {
+            HomeView()
+        } else {
+            VStack {
+                Spacer()
+                Image("AppLogo")
+                    .resizable()
+                    .frame(width: 300, height: 300)
+                Button(action: signIn) {
+                    Label("Se connecter", systemImage: "faceid")
+                }.buttonStyle(.borderedProminent).buttonBorderShape(.roundedRectangle).foregroundStyle(Color("loginBg"))
+                Spacer()
+            }.frame(maxWidth: .infinity).background(Color("loginBg"))
+        }
+        
     }
+    
+    func signIn() {
+        let context = LAContext()
+        var error: NSError?
+        
+        if (context.canEvaluatePolicy(.deviceOwnerAuthenticationWithBiometrics, error: &error)) {
+            let reason = "We need to unlock data"
+            context.evaluatePolicy(.deviceOwnerAuthenticationWithBiometrics, localizedReason: reason) { success, error in
+                if let error {
+                    print (error)
+                    return
+                }
+                if (success) {
+                    print("Authenticated.")
+                    isAuthentificated = success
+                }
+            }
+        }
+        
+    }
+
 }
 
 #Preview {
-    LoginView()
+    LoginView(isAuthentificated: .init(get: { false }, set: { _ in }))
 }
+
+
+
--- NutriLog/Screens/RootView.swift
+++ NutriLog/Screens/RootView.swift
@@ -2,13 +2,13 @@ import SwiftUI
 
 struct RootView: View {
     // TODO: Cette partie sera vue en classe pour l'expliquer comme il faut
-    @State private var isAuthenticated = true
+    @State private var isAuthenticated = false
     
     var body: some View {
         if (isAuthenticated) {
             HomeView()
         } else {
-            LoginView()
+            LoginView(isAuthentificated: $isAuthenticated)
         }
     }
 }
