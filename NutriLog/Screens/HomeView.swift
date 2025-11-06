4b8af3dc8d399c1c4fdbed5c6af3d89c9a48dd4a
Extrajulien <julien.lamothe01@gmail.com>
2025-11-06 11:28:26 -0500

    feat: add calories to home

--- NutriLog/Screens/HomeView.swift
+++ NutriLog/Screens/HomeView.swift
@@ -1,13 +1,48 @@
 import SwiftUI
 import SwiftData
+import Charts
 
 struct HomeView: View {
+    
+    private let maxCalories: Int = 2500
+    @State private var usedCalories: Int = 900
+    
     var body: some View {
-        Text("Vue Onglets pour Journée et Graphiques")
+        List {
+            Section(header: Text("Calories")) {
+                PercentageCircle(progress: Double(usedCalories)/Double(maxCalories), lineWidth: 8, size: 50)
+            }
+        }
     }
 }
 
 #Preview {
     HomeView()
         .modelContext(PersistenceController.preview.context)
+}
+
+struct PercentageCircle: View {
+    var progress: Double // 0.0 to 1.0
+    var lineWidth: CGFloat = 20
+    var color: Color = .green
+    var size: CGFloat = 80;
+    
+    var body: some View {
+        ZStack {
+            // Background circle (gray)
+            Circle()
+                .stroke(lineWidth: lineWidth)
+                .opacity(0.1)
+                .foregroundColor(color)
+            
+            // Foreground circle (progress)
+            Circle()
+                .trim(from: 0, to: progress)
+                .stroke(style: StrokeStyle(lineWidth: lineWidth, lineCap: .round))
+                .foregroundColor(color)
+                .rotationEffect(.degrees(-90)) // Start from top
+                .animation(.easeOut(duration: 0.5), value: progress)
+        }
+        .frame(width: size, height: size)
+    }
 }
